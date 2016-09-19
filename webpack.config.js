e4027cb5822021eff0559ab69950cb83cfb09bc7
hankaibo <hankaibo@hotmail.com>
2016-09-19 15:08:33 +0800

    chore:use webpack

---
+++ webpack.config.js
@@ -0,0 +1,190 @@
+// Helper: root() is defined at the bottom
+var path = require('path');
+var webpack = require('webpack');
+
+// Webpack Plugins
+var autoprefixer = require('autoprefixer');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+var CopyWebpackPlugin = require('copy-webpack-plugin');
+var DashboardPlugin = require('webpack-dashboard/plugin');
+
+/**
+ * Env
+ * Get npm lifecycle event to identify the environment
+ */
+var ENV = process.env.npm_lifecycle_event;
+var isProd = ENV === 'build';
+
+module.exports = function makeWebpackConfig() {
+  /**
+   * Config
+   * Reference: http://webpack.github.io/docs/configuration.html
+   * This is the object where all configuration gets set
+   */
+  var config = {};
+
+  /**
+   * Devtool
+   * Reference: http://webpack.github.io/docs/configuration.html#devtool
+   * Type of sourcemap to use per build type
+   */
+  if (isProd) {
+    config.devtool = 'source-map';
+  } else {
+    config.devtool = 'eval-source-map';
+  }
+
+  // add debug messages
+  config.debug = !isProd;
+
+  /**
+   * Entry
+   * Reference: http://webpack.github.io/docs/configuration.html#entry
+   */
+  config.entry = {
+    // our angular app
+  };
+
+  /**
+   * Output
+   * Reference: http://webpack.github.io/docs/configuration.html#output
+   */
+  config.output = {
+    path: root('dist'),
+    publicPath: isProd ? '/' : 'http://localhost:8080/',
+    filename: isProd ? 'js/[name].[hash].js' : 'js/[name].js',
+    chunkFilename: isProd ? '[id].[hash].chunk.js' : '[id].chunk.js'
+  };
+
+  /**
+   * Resolve
+   * Reference: http://webpack.github.io/docs/configuration.html#resolve
+   */
+  config.resolve = {
+    cache: true,
+    root: root(),
+    // only discover files that have those extensions
+    extensions: ['', '.css', '.scss'],
+    alias: {}
+  };
+
+  /**
+   * Loaders
+   * Reference: http://webpack.github.io/docs/configuration.html#module-loaders
+   * List: http://webpack.github.io/docs/list-of-loaders.html
+   * This handles most of the magic responsible for converting modules
+   */
+  config.module = {
+    preLoaders: [],
+    loaders: [
+      // Support for CSS as raw text
+      // use 'null' loader in test mode (https://github.com/webpack/null-loader)
+      // all css in src/style will be bundled in an external css file
+      {
+        test: /\.css$/,
+        exclude: root('src', 'app'),
+        loader: ExtractTextPlugin.extract('style', 'css?sourceMap!postcss')
+      },
+      // all css required in src/app files will be merged in js files
+      { test: /\.css$/, include: root('src', 'app'), loader: 'raw!postcss' },
+
+      // support for .scss files
+      // use 'null' loader in test mode (https://github.com/webpack/null-loader)
+      // all css in src/style will be bundled in an external css file
+      {
+        test: /\.scss$/,
+        exclude: root('src', 'app'),
+        loader: ExtractTextPlugin.extract('style', 'css?sourceMap!postcss!sass')
+      },
+      // all css required in src/app files will be merged in js files
+      { test: /\.scss$/, exclude: root('src', 'style'), loader: 'raw!postcss!sass' }
+    ],
+    postLoaders: []
+  };
+  /**
+   * Plugins
+   * Reference: http://webpack.github.io/docs/configuration.html#plugins
+   * List: http://webpack.github.io/docs/list-of-plugins.html
+   */
+  config.plugins = [
+    // Define env variables to help with builds
+    // Reference: https://webpack.github.io/docs/list-of-plugins.html#defineplugin
+    new webpack.DefinePlugin({
+      // Environment helpers
+      'process.env': {
+        ENV: JSON.stringify(ENV)
+      }
+    }),
+    new DashboardPlugin(),
+    // Extract css files
+    // Reference: https://github.com/webpack/extract-text-webpack-plugin
+    // Disabled when in test mode or not in build mode
+    new ExtractTextPlugin('css/[name].[hash].css', { disable: !isProd }),
+    //把指定文件夹下的文件复制到指定的目录
+    // new TransferWebpackPlugin([
+    //   { from: 'src' }
+    // ], root('dist'))
+  ];
+
+  // Add build specific plugins
+  if (isProd) {
+    config.plugins.push(
+      // Reference: http://webpack.github.io/docs/list-of-plugins.html#noerrorsplugin
+      // Only emit files when there are no errors
+      new webpack.NoErrorsPlugin(),
+
+      // Reference: http://webpack.github.io/docs/list-of-plugins.html#dedupeplugin
+      // Dedupe modules in the output
+      new webpack.optimize.DedupePlugin(),
+
+      // Reference: http://webpack.github.io/docs/list-of-plugins.html#uglifyjsplugin
+      // Minify all javascript, switch loaders to minimizing mode
+      new webpack.optimize.UglifyJsPlugin({ mangle: { keep_fnames: true } }),
+
+      // Copy assets from the public folder
+      // Reference: https://github.com/kevlened/copy-webpack-plugin
+      new CopyWebpackPlugin([{
+        from: root('src')
+      }])
+    );
+  }
+
+  /**
+   * PostCSS
+   * Reference: https://github.com/postcss/autoprefixer-core
+   * Add vendor prefixes to your css
+   */
+  config.postcss = [
+    autoprefixer({
+      browsers: ['last 2 version']
+    })
+  ];
+
+  /**
+   * Sass
+   * Reference: https://github.com/jtangelder/sass-loader
+   * Transforms .scss files to .css
+   */
+  config.sassLoader = {
+    //includePaths: [path.resolve(__dirname, "node_modules/foundation-sites/scss")]
+  };
+  /**
+   * Dev server configuration
+   * Reference: http://webpack.github.io/docs/configuration.html#devserver
+   * Reference: http://webpack.github.io/docs/webpack-dev-server.html
+   */
+  config.devServer = {
+    contentBase: '.',
+    historyApiFallback: true,
+    quiet: true,
+    stats: 'minimal' // none (or false), errors-only, minimal, normal (or true) and verbose
+  };
+
+  return config;
+} ();
+
+// Helper functions
+function root(args) {
+  args = Array.prototype.slice.call(arguments, 0);
+  return path.join.apply(path, [__dirname].concat(args));
+}
